caab62245cb0a8ae025aba0710ab4ee4a4338fb9
jcarmonaedu <33189340+jcarmonaedu@users.noreply.github.com>
2018-10-22 23:54:36 -0300

    Update FormaPagoDTO.java

--- src/java/app/domain/FormaPagoDTO.java
+++ src/java/app/domain/FormaPagoDTO.java
@@ -43,35 +43,4 @@ public class FormaPagoDTO
 	{
 		this.recargo=recargo;
 	}
-
-	public List<FormaPagoDTO> getFormasPago()
-	{
-		// el query
-		String sql="";
-		sql+="SELECT id_forma_pago ";
-		sql+="      ,descripcion ";
-		sql+="      ,recargo ";
-		sql+="FROM forma_pago ";
-
-		// ejecuto el query
-		try (Connection con=UConnection.getConnection(); PreparedStatement pstm=con.prepareStatement(sql); ResultSet rs=pstm.executeQuery())
-		{
-			List<FormaPagoDTO> lst=new ArrayList<>();
-			while(rs.next())
-			{
-				FormaPagoDTO dto=new FormaPagoDTO();
-				dto.setIdFormaPago(rs.getInt("id_forma_pago"));
-				dto.setDescripcion(rs.getString("descripcion"));
-				dto.setRecargo(rs.getDouble("recargo"));
-				lst.add(dto);
-			}
-
-			return lst;
-		}
-		catch(Exception ex)
-		{
-			ex.printStackTrace();
-			throw new RuntimeException(ex);
-		}
-	}
 }
